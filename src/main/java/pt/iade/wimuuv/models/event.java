6cae20545aaa0ee37f2039b7bdf9beed4de880a9
Luís Silva <ifaf.silva12@gmail.com>
2021-11-13 21:10:06 +0000

    commit

--- src/main/java/pt/iade/wimuuv/models/event.java
+++ src/main/java/pt/iade/wimuuv/models/event.java
@@ -1,12 +1,17 @@
 package pt.iade.wimuuv.models;
 
 
+import java.sql.Date;
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import net.bytebuddy.asm.Advice.Local;
 
 @Entity
 @Table(name="event")
@@ -17,10 +22,10 @@ public class event {
     @Column(name="event_name") private String event_name;
     @Column(name="event_description") private String event_description;
     @Column(name="event_type") private String event_type;
-    @Column(name="event_date") private int event_date;
-    @Column(name="event_starttime") private int event_starttime;
-    @Column(name="event_endtime") private int event_endtime;
-    @Column(name="event_duration") private int event_duration;
+    @Column(name="event_date") private LocalDate event_date;
+    @Column(name="event_starttime") private LocalDate event_starttime;
+    @Column(name="event_endtime") private LocalDate event_endtime;
+    @Column(name="event_duration") private double event_duration;
     @Column(name="event_org") private int event_org;
     @Column(name="event_spot ") private int event_spot ;
     @Column(name="event_capacity") private int event_capacity;
@@ -32,7 +37,7 @@ public class event {
     public int getEvent_capacity() {
         return event_capacity;
     }
-    public int getEvent_date() {
+    public LocalDate getEvent_date() {
         return event_date;
     }
     public String getEvent_description() {
