918a99d3fc54d2cb68701597f5a58838f9511cc1
Luis Silva <ifaf.silva12@gmail.com>
2021-11-14 19:15:19 +0000

    commit

--- src/main/java/pt/iade/wimuuv/models/repositories/photoRepository.java
+++ src/main/java/pt/iade/wimuuv/models/repositories/photoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import pt.iade.wimuuv.models.photo;
 
 public interface PhotoRepository extends CrudRepository<photo, Integer> {
-    
+    asdasd
 }
