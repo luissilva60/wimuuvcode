f838edb9d94d6cad740d8f913e81398e70aa93f0
Luis Silva <ifaf.silva12@gmail.com>
2021-11-17 07:16:33 +0000

    commit

--- src/main/java/pt/iade/wimuuv/models/student_course.java
+++ src/main/java/pt/iade/wimuuv/models/student_course.java
@@ -13,7 +13,7 @@ public class student_course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="stu_course_id") private int stu_course_id;
     @Column(name="stu_course_name") private String stu_course_name;
-    @Column(name="stu_course_year") private String stu_course_year;
+    
 
     public int getStu_course_id() {
         return stu_course_id;
@@ -21,7 +21,5 @@ public class student_course {
     public String getStu_course_name() {
         return stu_course_name;
     }
-    public String getStu_course_year() {
-        return stu_course_year;
-    }
+    
 }
