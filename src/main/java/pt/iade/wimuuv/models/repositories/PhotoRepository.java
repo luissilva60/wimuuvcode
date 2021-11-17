939a0225f15c2656290c50193594a998e432f5af
luissilva60 <90901919+luissilva60@users.noreply.github.com>
2021-11-17 21:41:58 +0000

    Add files via upload

---
+++ src/main/java/pt/iade/wimuuv/models/repositories/PhotoRepository.java
@@ -0,0 +1,10 @@
+package pt.iade.wimuuv.models.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import pt.iade.wimuuv.models.photo;
+
+public interface PhotoRepository extends CrudRepository<photo, Integer>{
+    
+}
+
