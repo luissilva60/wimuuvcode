4927331b09efd55bb7b604b4f1a46590cc67f7dd
Luís Silva <ifaf.silva12@gmail.com>
2021-11-18 15:15:40 +0000

    commit

--- src/main/java/pt/iade/wimuuv/models/student.java
+++ src/main/java/pt/iade/wimuuv/models/student.java
@@ -16,39 +16,39 @@ public class student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="stu_id") private int id;
-    @Column(name="stu_name") private String stu_name;
-    @Column(name="stu_email") private String stu_email;
-    @Column(name="stu_password") private String stu_password;
-    @Column(name="stu_bdate") private LocalDate stu_bdate;
-    @Column(name="stu_gender") private char stu_gender;
-    @Column(name="stu_crse_id") private int stu_crse_id;
-    @Column(name="stu_photo_id") private int stu_photo_id;
+    @Column(name="stu_name") private String name;
+    @Column(name="stu_email") private String email;
+    @Column(name="stu_password") private String password;
+    @Column(name="stu_bdate") private LocalDate bdate;
+    @Column(name="stu_gender") private char gender;
+    @Column(name="stu_crse_id") private int crseId;
+    @Column(name="stu_photo_id") private int photoId;
     public student() {}
-    public LocalDate getStu_Bdate() {
-        return stu_bdate;
+    public LocalDate getBdate() {
+        return bdate;
     }
 
     public int getCurrentAge() {
         LocalDate now = LocalDate.now();
-        return Period.between(stu_bdate, now).getYears();
+        return Period.between(bdate, now).getYears();
     }
-    public char getStu_Gender() {
-        return stu_gender;
+    public char getGender() {
+        return gender;
     }
     public int getId() {
         return id;
     }
-    public String getStu_email() {
-        return stu_email;
+    public String getEmail() {
+        return email;
     }
-    public String getStu_Name() {
-        return stu_name;
+    public String getName() {
+        return name;
     }
-    public int getStu_crse_id() {
-        return stu_crse_id;
+    public int getcrseId() {
+        return crseId;
     }
-    public int getStu_photo_id() {
-        return stu_photo_id;
+    public int getphotoId() {
+        return photoId;
     }
 
     
