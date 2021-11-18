d9b91ce76a2a0a182b98d9ba12d6e51c59509e5f
Luís Silva <ifaf.silva12@gmail.com>
2021-11-18 14:24:29 +0000

    commit

--- src/main/java/pt/iade/wimuuv/models/repositories/StudentRepository.java
+++ src/main/java/pt/iade/wimuuv/models/repositories/StudentRepository.java
@@ -14,8 +14,8 @@ import pt.iade.wimuuv.models.student;
 public interface StudentRepository extends CrudRepository<student, Integer>{
     @Modifying @Transactional
     @Query(value="insert into student (stu_name,stu_email, stu_photo_id,stu_password,stu_bdate,stu_gender,stu_crse_id)"+
-        "values(:#{#student.stu}, :#{#student.stu_email}, :#{#student.stu_photo_id}, :#{#student.stu_password}, :#{#student.stu_bdate}"+
-        ",:#{#student.stu_gender}, :#{#student.stu_crse_id} )", nativeQuery=true)
+        "values(:#{#student.stu_name}, :#{#student.stu_email}, :#{#student.stu_photo_id}, :#{#student.stu_password}, :#{#student.stu_bdate}"+
+        ", :#{#student.stu_gender}, :#{#student.stu_crse_id} )", nativeQuery=true)
     Integer registerStudent(@Param("student") student student);
                         
 }
