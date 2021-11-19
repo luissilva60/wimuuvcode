0a61d300642c3481ae4127cf72320d24b2be2bf5
Luis Silva <ifaf.silva12@gmail.com>
2021-11-19 01:28:29 +0000

    commit

--- src/main/java/pt/iade/wimuuv/models/repositories/Student_RateRepository.java
+++ src/main/java/pt/iade/wimuuv/models/repositories/Student_RateRepository.java
@@ -7,9 +7,9 @@ import pt.iade.wimuuv.models.student_rate;
 
 
 public interface Student_RateRepository extends CrudRepository<student_rate, Integer> {
-    String resQuery1 = "select stu_rate_ev, org_name, event_name, spot_name, stu_id"
-    + "from student_rate, org, event, spot, student" +
-    "where event_id = 2" +
+    String resQuery1 = "select stu_rate_ev, org_name, event_name, spot_name, stu_id "
+    + "from student_rate, org, event, spot, student " +
+    "where event_id = 2 " +
     "order by stu_rate_ev DESC";
 
     @Query(value=resQuery1, nativeQuery=true)
