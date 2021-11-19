6b6b67c91d4c789718102d93a4b741d16fa1fd06
Luis Silva <ifaf.silva12@gmail.com>
2021-11-19 00:42:32 +0000

    commit

--- src/main/java/pt/iade/wimuuv/models/repositories/EventRepository.java
+++ src/main/java/pt/iade/wimuuv/models/repositories/EventRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.repository.CrudRepository;
 import pt.iade.wimuuv.models.event;
 
 public interface EventRepository extends CrudRepository<event, Integer> {
-    String resQuery1 = "select event_name from event " + 
-                    "where event_org_id = 1";
+    String resQuery1 = "select event_name, spot_name "+
+    "from event, spot" + "where event_org_id = 1";
 
     @Query(value=resQuery1, nativeQuery=true)
     Iterable<String>getEventOrgId1();
