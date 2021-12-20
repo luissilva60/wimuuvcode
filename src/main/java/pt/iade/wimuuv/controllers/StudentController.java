d8c2ce976cd9fb6ffc299f6eece28868a10581ae
Luis Silva <ifaf.silva12@gmail.com>
2021-12-20 17:33:40 +0000

    c

--- src/main/java/pt/iade/wimuuv/controllers/StudentController.java
+++ src/main/java/pt/iade/wimuuv/controllers/StudentController.java
@@ -63,7 +63,7 @@ public class StudentController {
         return studentRepository.SignupStudent(name, email,password, bdate, gender,course_id );
     }
 
-    @GetMapping(path = "/{id:[0-9]+}/changepw/{password}", produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(path = "/{id:[0-9]+}/changepw/{password}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Iterable<student> ChangePasswordStudent(@PathVariable String password, int id) {
         logger.info("Sending all events with org id: " + id);
         return studentRepository.ChangePasswordStudent(password, id);
